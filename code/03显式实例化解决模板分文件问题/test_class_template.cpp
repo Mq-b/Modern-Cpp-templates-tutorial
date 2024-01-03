aef0e3783c1bfd14746926f02dfa9d0c9173621c
归故里 <3326284481@qq.com>
2024-01-03 10:58:11 +0800

    1. 修改注释，强调显式实例化的是成员函数

--- code/03显式实例化解决模板分文件问题/test_class_template.cpp
+++ code/03显式实例化解决模板分文件问题/test_class_template.cpp
@@ -5,4 +5,4 @@ void N::X<T>::f(){
     std::cout << "f: " << typeid(T).name() << '\n';
 }
 
-template void N::X<int>::f();    // 显式实例化定义
+template void N::X<int>::f();    // 显式实例化定义 成员函数，这不是显式实例化类模板
