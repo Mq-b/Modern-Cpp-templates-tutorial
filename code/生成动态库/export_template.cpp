b405a7c2e878bf9b6bd1e3b106323b0d1ee5eb92
归故里 <3326284481@qq.com>
2024-01-08 15:03:58 +0800

    修改模板生成动态库的代码

--- code/生成动态库/export_template.cpp
+++ code/生成动态库/export_template.cpp
@@ -4,3 +4,13 @@ template<typename T>
 void f(T) {
     std::cout << typeid(T).name() << '\n';
 }
+
+template <typename T>
+void X<T>::f(){
+    std::cout << typeid(T).name() << '\n';
+}
+
+template __declspec(dllexport)  void f<int>(int);
+template __declspec(dllexport)  void f<std::string>(std::string);
+
+template struct X<int>;
--- code/生成动态库/export_template.h
+++ code/生成动态库/export_template.h
@@ -6,12 +6,7 @@
 template<typename T>
 void f(T);
 
-template __declspec(dllexport)  void f<int>(int);
-template __declspec(dllexport)  void f<std::string>(std::string);
-
 template<typename T>
 struct __declspec(dllexport) X {
-    void f() { std::cout << typeid(T).name() << '\n'; }
+    void f();
 };
-
-template struct X<int>;
