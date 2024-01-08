c69ac34ab70f5d10ad643a16a7f637ca437c4826
归故里 <3326284481@qq.com>
2024-01-08 10:00:24 +0800

    1. 删除不能跨平台的二进制动态库 2. 完成了模板生成动态库的内容，函数模板、类模板 3.增加07的描述

--- code/05显式实例化解决模板导出动态静态库问题/export_template.h
+++ code/05显式实例化解决模板导出动态静态库问题/export_template.h
@@ -8,3 +8,10 @@ void f(T);
 
 template __declspec(dllexport)  void f<int>(int);
 template __declspec(dllexport)  void f<std::string>(std::string);
+
+template<typename T>
+struct __declspec(dllexport) X {
+    void f() { std::cout << typeid(T).name() << '\n'; }
+};
+
+template struct X<int>;
