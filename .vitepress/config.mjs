9849b6e265a0bb43884b6d51fd33258e9c83c709
Suzukaze <1027743497@qq.com>
2024-03-18 18:30:34 +0800

    add vitepress

---
+++ .vitepress/config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vitepress'
+
+const tutorial_path = '/md/第一部分-基础知识/';
+const homework_path = '/homework/08折叠表达式作业/';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "现代 C++ 模板教程",
+  rewrites: {
+    'README.md': 'index.md'
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/image/icon.webp',
+    base: '/Modern-Cpp-templates-tutorial/',
+    sidebar: [
+      {
+        items: [
+          { text: '首页', link: '/README' },
+          { text: '函数模板', link: '/md/README' },
+          { text: '阅读须知', link: tutorial_path + '01函数模板' },
+          { text: '类模板', link: tutorial_path + '02类模板' },
+          { text: '变量模板', link: tutorial_path + '03变量模板' },
+          { text: '模板全特化', link: tutorial_path + '04模板全特化' },
+          { text: '模板偏特化', link: tutorial_path + '05模板偏特化' },
+          { text: '模板显式实例化解决模板分文件问题', link: tutorial_path + '06模板显式实例化解决模板分文件问题' },
+          { text: '显式实例化解决模板导出静态动态库', link: tutorial_path + '07显式实例化解决模板导出静态动态库' },
+          { text: '折叠表达式', link: tutorial_path + '08折叠表达式' },
+          { text: '待决名', link: tutorial_path + '09待决名' },
+          { text: '了解与利用SFINAE', link: tutorial_path + '10了解与利用SFINAE' },
+          { text: '约束与概念', link: tutorial_path + '11约束与概念' },
+          {
+            text: '作业提交展示',
+            collapsed: true,
+            items: [
+              { text: 'mq卢瑟', link: homework_path + 'mq卢瑟' },
+              { text: 'roseveknif', link: homework_path + 'roseveknif' },
+              { text: 'mq日', link: homework_path + 'mq日' },
+              { text: 'saidfljwnzjasf', link: homework_path + 'saidfljwnzjasf' },
+              { text: 'ooolize', link: homework_path + 'ooolize' },
+            ]
+          },
+          { text: '总结', link: '/md/第一部分-基础知识/12总结' },
+        ]
+      },
+    ],
+    returnToTopLabel: '回到顶端',
+    editLink: {
+      pattern: 'https://github.com/Mq-b/Modern-Cpp-templates-tutorial',
+      text: '在 GitHub 上编辑此页面'
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Mq-b/Modern-Cpp-templates-tutorial' },
+    ]
+  }
+})
